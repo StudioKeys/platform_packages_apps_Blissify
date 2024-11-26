0f6d3855d1e839b8675c6f499aa93f3e3e7cb22c
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-26 00:40:01 -0500

    Blissify: Add support for json-based (PIF) spoofing[2/2]
    Change-Id: Iadc41f9b3d921e61096f94aee0a44c5d78a9a28d
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/org/blissroms/blissify/fragments/misc/Spoofing.java
+++ src/org/blissroms/blissify/fragments/misc/Spoofing.java
@@ -5,10 +5,15 @@
 
 package org.blissroms.blissify.fragments.misc;
 
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
@@ -21,6 +26,10 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
 
+import org.json.JSONObject;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 import java.util.List;
 
 import com.bliss.support.preferences.SystemPropertySwitchPreference;
@@ -34,12 +43,17 @@ public class Spoofing extends SettingsPreferenceFragment implements
     private static final String KEY_SYSTEM_WIDE_CATEGORY = "spoofing_system_wide_category";
     private static final String KEY_PIXEL_PROPS = "persist.sys.pphooks.enable";
 
+    private static final String KEY_PIF_JSON_FILE_PREFERENCE = "pif_json_file_preference";
+    private Preference mPifJsonFilePreference;
+    private Handler mHandler;
+
     private PreferenceCategory mSystemWideCategory;
     private SystemPropertySwitchPreference mPixelProps;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mHandler = new Handler();
         addPreferencesFromResource(R.xml.spoofing);
 
         final Context context = getContext();
@@ -49,6 +63,42 @@ public class Spoofing extends SettingsPreferenceFragment implements
 
         mSystemWideCategory = (PreferenceCategory) findPreference(KEY_SYSTEM_WIDE_CATEGORY);
         mPixelProps = (SystemPropertySwitchPreference) findPreference(KEY_PIXEL_PROPS);
+
+        mPifJsonFilePreference = findPreference(KEY_PIF_JSON_FILE_PREFERENCE);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mPifJsonFilePreference) {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("application/json");
+            startActivityForResult(intent, 10001);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10001 && resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            Log.d(TAG, "URI received: " + uri.toString());
+            try (InputStream inputStream = getActivity().getContentResolver().openInputStream(uri)) {
+                if (inputStream != null) {
+                    String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+                    Log.d(TAG, "JSON data: " + json);
+                    JSONObject jsonObject = new JSONObject(json);
+                    for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
+                        String key = it.next();
+                        String value = jsonObject.getString(key);
+                        Log.d(TAG, "Setting property: persist.sys.pihooks_" + key + " = " + value);
+                        SystemProperties.set("persist.sys.pihooks_" + key, value);
+                    }
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "Error reading JSON or setting properties", e);
+            }
+        }
     }
 
     @Override
